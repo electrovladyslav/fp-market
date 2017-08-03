3a6804d5b792a4d4e0afd84f4ab4528629e854fb
Yermolin Vlad <electrovladyslav@gmail.com>
2017-08-03 12:36:02 +0300

    Update 2017-08-03T09:35:58.657Z

--- js/main.js
+++ js/main.js
@@ -1,34 +1,5 @@
-(function () {
-'use strict';
+import StockView from "./StockView";
 
-const getResponse = (url) => new Promise(
-  (resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.onload = (evt) =>
-      resolve(evt.target.response);
-    xhr.send();
-  }
-);
-getResponse('http://webtask.future-processing.com:8068/stocks').then((data) => console.log(data));
-/*
-
- const xhr = new XMLHttpRequest();
-
- xhr.open('GET', 'http://webtask.future-processing.com:8068/stocks', true);
-
- xhr.send();
-
- xhr.onreadystatechange = function() {
- if (xhr.readyState != 4) return;
-
- if (xhr.status != 200) {
- console.log(xhr.status + ': ' + xhr.statusText);
- } else {
- console.log(xhr.responseText);
- }
- }*/
-
-}());
-
+const stock = new StockView();
+stock.render();
 //# sourceMappingURL=main.js.map
